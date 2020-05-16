1f8066bb30cd6454a0934abd563c575e13bde6af
Sebastian Paarmann <sebastian.paarmann@tuhh.de>
2020-05-16 16:17:51 +0200

    Add operators test file

---
+++ tests/0-operators.cc
@@ -0,0 +1,21 @@
+int main() {
+  throw throw error;
+  **test = 'a';
+
+  ++ ++ a;
+  a ++ ++;
+  - - a;
+
+  //;
+
+  char test;
+
+  return 3 * (2 + 5 / 9) * 78 % 8 >> "hello" << lol[5 - 3 + (-3)]
+    <=> "somestuff"[123%32] - 123 << "33" + (som->heh(xyz + 1)
+      ? ething.huh->blub.a("hello"[23 - "hm"])
+      : bla.mmm(ok));
+
+  return a ? b ? x : y : z;
+  int a = b;
+  throw ++a++ += b -- + std::defvalue -= 8++ + 10 != x && y || b != 3 < 5 <= b >= 3;
+}
