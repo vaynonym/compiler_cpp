64c6f8fa3c9a338a18f1bf196e0e7225d5306203
Sebastian Paarmann <sebastian.paarmann@tuhh.de>
2020-05-17 22:58:32 +0200

    Add more templates test

--- tests/0-templates.cc
+++ tests/0-templates.cc
@@ -2,4 +2,7 @@ int main(){
     std::Vector2<int> test;
     std::map<int, vector<string>> mit;
     std::vector<int>::vector<int>::size_type ost;
+    std::vector<vector<vector<int>>> baz;
+    std::vector<vector<vector<vector<int>>>> bar;
+    std::map<int, std::vector<string>> foo;
 }
