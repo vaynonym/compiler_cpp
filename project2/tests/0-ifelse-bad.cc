c9c91e23b4ba942733c639f75857061e42730258
Kristina <kristina.schrapers@tuhh.de>
2020-06-06 20:25:59 +0200

    [#12] Added Tests for If Else Statements

---
+++ project2/tests/0-ifelse-bad.cc
@@ -0,0 +1,33 @@
+int main(){
+    int a=5;
+    
+
+    if(a){
+        true++;
+    }
+    else{
+       false++;
+    }
+    bool b=true;
+    if(2.5){
+       a--;
+    }
+    else{
+        a++;
+    }
+    
+    if(true){
+        a/b;
+    }
+    else{
+       false++;
+    }
+     
+    if(true){
+        a++;
+    }
+    else{
+        true++;
+    }
+ return 0;
+}
---
+++ project2/tests/0-ifelse-good.cc
@@ -0,0 +1,17 @@
+int main(){
+    int a=5;
+    if(a==5){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+    bool b=true;
+    if(b){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+
+}
