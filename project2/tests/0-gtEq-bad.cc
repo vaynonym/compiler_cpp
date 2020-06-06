e4bb45eb18930ef8b241ccd6d90d030db405986f
Kristina <kristina.schrapers@tuhh.de>
2020-06-06 20:43:26 +0200

    [#12] added Tests for >=

---
+++ project2/tests/0-gtEq-bad.cc
@@ -0,0 +1,9 @@
+int main(){
+    bool a= false;
+    double b=4.0;
+    a>=b;
+    b>=a;
+    a>=a;
+    b>=b;
+    return 0;
+}
---
+++ project2/tests/0-gtEq-good.cc
@@ -0,0 +1,11 @@
+int main(){
+    int a=0;
+    double b=4.0;
+    a>=b;
+    b>=a;
+    a>=a;
+    b>=b;
+    9.0>=1.0;
+
+    return 0;
+}
