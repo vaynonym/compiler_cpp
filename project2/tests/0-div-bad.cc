4083a4bf78175a8f325ca50fea166c2b4a0c3ac7
Kristina <kristina.schrapers@tuhh.de>
2020-06-06 19:57:40 +0200

    [#12] added Tests for Division

---
+++ project2/tests/0-div-bad.cc
@@ -0,0 +1,10 @@
+int main(){
+    bool a=true;
+    int b= 5;
+    double c=1.0;
+    a/b;
+    b/a;
+    c/a;
+    a/c;
+    return 0;
+}
---
+++ project2/tests/0-div-good.cc
@@ -0,0 +1,9 @@
+int main(){
+    int a = 6;
+    double b=2.5;
+    a/b;
+    b/a;
+    a/a;
+    b/b;
+    return 0;
+}
